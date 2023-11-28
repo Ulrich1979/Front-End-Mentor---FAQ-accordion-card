a37f78c37760cf8099838d335a638e79beeea51a
Ulrich1979 <141607646+Ulrich1979@users.noreply.github.com>
2023-11-28 22:10:31 +0100

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,20 @@
+// SWITCH OF ACTIVE STATES OF ELEMENT //
+
+var acc = document.getElementsByClassName("accordeon");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
